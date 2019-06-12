c19f91cdafb7efe5e9c3d9f701cadd3d7675cd99
Jilong Xue <jxue@microsoft.com>
2019-06-12 18:47:45 +0900

    fix compilation error and code style

--- src/nnfusion/core/kernels/cuda_gpu/kernels/batch_matmul.hpp
+++ src/nnfusion/core/kernels/cuda_gpu/kernels/batch_matmul.hpp
@@ -3,7 +3,7 @@
 
 #include "../cuda_emitter.hpp"
 #include "../cuda_langunit.hpp"
-#include "../../../ops/generic_op.hpp"
+#include "nnfusion/core/ops/generic_op.hpp"
 
 /*********************************
 >> For Config detail, please reference ../../../ops/op_registration.cpp
@@ -19,7 +19,6 @@ Example:
     }
 *********************************/
 
-
 namespace nnfusion
 {
     namespace kernels
@@ -28,40 +27,45 @@ namespace nnfusion
         {
             class BatchMatmul : public CudaEmitter
             {
-				shared_ptr<ngraph::op::GenericOp> generic_op;
+                shared_ptr<ngraph::op::GenericOp> generic_op;
 
             public:
-                BatchMatmul(shared_ptr<KernelContext> ctx) : CudaEmitter(ctx), generic_op(static_pointer_cast<ngraph::op::GenericOp>(ctx->node)) {
-				}
+                BatchMatmul(shared_ptr<KernelContext> ctx)
+                    : CudaEmitter(ctx)
+                    , generic_op(static_pointer_cast<ngraph::op::GenericOp>(ctx->node))
+                {
+                }
 
-                LanguageUnit_p emit_function_body() override {
-					LanguageUnit_p _lu(new LanguageUnit(get_function_name()));
-					auto& lu = *_lu;
-					// function signature:
-					// extern "C" __global__ void kernel(m_context->dtypes[0]* input0, m_context->dtypes[0]* input1, m_context->dtypes[2]* output0)
-					lu.block_begin();
-					lu << "// hello text for batch_matmul, " << generic_op->localOpConfig.get("adj_x")["b"] << ";\n";
-					lu.block_end();
-					return _lu;
-				}
+                LanguageUnit_p emit_function_body() override
+                {
+                    LanguageUnit_p _lu(new LanguageUnit(get_function_name()));
+                    auto& lu = *_lu;
+                    // function signature:
+                    // extern "C" __global__ void kernel(m_context->dtypes[0]* input0, m_context->dtypes[0]* input1, m_context->dtypes[2]* output0)
+                    lu.block_begin();
+                    lu << "// hello text for batch_matmul, "
+                       << generic_op->localOpConfig.get("adj_x")["b"] << ";\n";
+                    lu.block_end();
+                    return _lu;
+                }
 
-                void set_launch_config() override {
-					// Just for test currently
-					m_gridDim = dim3(4, 1, 1);
-					m_blockDim = dim3(64, 1, 1);
-				}
+                void set_launch_config() override
+                {
+                    // Just for test currently
+                    m_gridDim = dim3(4, 1, 1);
+                    m_blockDim = dim3(64, 1, 1);
+                }
 
-                LanguageUnit_p emit_dependency() override {
-					LanguageUnit_p _lu(new LanguageUnit(get_function_name() + "_dep"));
-					_lu->require(header::cuda);
-					return _lu;
-				}
-
+                LanguageUnit_p emit_dependency() override
+                {
+                    LanguageUnit_p _lu(new LanguageUnit(get_function_name() + "_dep"));
+                    _lu->require(header::cuda);
+                    return _lu;
+                }
             };
         } // namespace cuda
     }     // namespace kernels
 } // namespace nnfusion
-
 
 // Register Pad kernel emitter
 
@@ -71,4 +75,3 @@ using namespace nnfusion::kernels;
 REGISTER_KERNEL_EMITTER("BatchMatmul",                                                // op_name
                         Device(CUDA_GPU).TypeConstraint(DT_FLOAT).Tag("cuda_kernel"), // attrs
                         cuda::BatchMatmul)                                            // constructor
-
