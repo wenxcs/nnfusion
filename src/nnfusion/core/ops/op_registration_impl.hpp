d4af29b9958d0500160d6fdd76d8f104eb57807a
Wei CUI <weicu@microsoft.com>
2019-06-13 18:54:37 +0000

    move `get_op_configs()` into impl header

---
+++ src/nnfusion/core/ops/op_registration_impl.hpp
@@ -0,0 +1,15 @@
+// Microsoft (c) 2019, NNFusion Team
+#pragma once
+
+#include "nnfusion/core/ops/generic_op.hpp"
+
+namespace ngraph {
+
+    namespace op {
+
+        std::unordered_map<std::string, ngraph::op::OpConfig> &get_op_configs() {
+            static std::unordered_map<std::string, ngraph::op::OpConfig> __op_configs;
+            return __op_configs;
+        }
+    }
+}
