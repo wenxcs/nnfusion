81b162daf3d6f9367dce700ec8bf00956cce77e0
Jilong Xue <jxue@microsoft.com>
2019-06-13 18:16:26 +0900

    code style

--- src/nnfusion/core/kernels/cuda_gpu/kernels/anyop.cpp
+++ src/nnfusion/core/kernels/cuda_gpu/kernels/anyop.cpp
@@ -37,6 +37,6 @@ LanguageUnit_p cuda::AnyOP::emit_dependency()
 
 // Register Pad kernel emitter
 
-REGISTER_KERNEL_EMITTER("AnyOP",                                                      //op_name
-                        Device(CUDA_GPU).TypeConstraint(DT_FLOAT),                    //attrs
-                        cuda::AnyOP)                                                  // constructor
+REGISTER_KERNEL_EMITTER("AnyOP",                                   //op_name
+                        Device(CUDA_GPU).TypeConstraint(DT_FLOAT), //attrs
+                        cuda::AnyOP)                               // constructor
