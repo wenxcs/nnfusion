14917c33e68b02a037323c67801bf6be91f301fc
yuqxia <yuqxia@microsoft.com>
2019-09-06 18:57:26 +0800

    fix format

--- src/nnfusion/core/graph/pass/op_inplace_pass.cpp
+++ src/nnfusion/core/graph/pass/op_inplace_pass.cpp
@@ -1,21 +1,20 @@
 // Microsoft (c) 2019, NNFusion Team
 
-
 #include "op_inplace_pass.hpp"
 #include "../gnode.hpp"
 #include "../graph.hpp"
+#include "ngraph/op/broadcast.hpp"
 #include "ngraph/op/reshape.hpp"
 #include "ngraph/op/result.hpp"
 #include "ngraph/op/sum.hpp"
-#include "ngraph/op/broadcast.hpp"
 
 using namespace nnfusion::graph;
 using namespace nnfusion::graph::pass;
 
 bool OpInplacePass::run_on_graph(std::shared_ptr<Graph>& graph)
-{    
+{
     for (auto node : graph->get_nodes())
-    {   
+    {
         // add inplace tag for reshape op if !op->get_is_transpose() || op element num < 2
         if (node->get_op_type() == "Reshape")
         {
@@ -50,19 +49,19 @@ bool OpInplacePass::run_on_graph(std::shared_ptr<Graph>& graph)
 
             auto op_annotations = result->get_op_annotations();
             if (op_annotations)
-                {
-                    // pass-through
-                    op_annotations->add_in_place_oi_pair({0, 0, false});
-                }
-                else
-                {
-                    op_annotations = std::make_shared<ngraph::op::util::OpAnnotations>();
-                    // pass-through
-                    op_annotations->add_in_place_oi_pair({0, 0, false});
-                    result->set_op_annotations(op_annotations);
-                }               
+            {
+                // pass-through
+                op_annotations->add_in_place_oi_pair({0, 0, false});
+            }
+            else
+            {
+                op_annotations = std::make_shared<ngraph::op::util::OpAnnotations>();
+                // pass-through
+                op_annotations->add_in_place_oi_pair({0, 0, false});
+                result->set_op_annotations(op_annotations);
+            }
         }
-        
+
         if (node->get_op_type() == "Sum")
         {
             std::shared_ptr<ngraph::op::Sum> sum =
