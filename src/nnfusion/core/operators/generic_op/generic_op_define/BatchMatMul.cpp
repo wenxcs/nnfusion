93ca84d0290022be7d54c135e862781f5e7ea461
Wei Cui <weicu@microsoft.com>
2020-05-20 10:28:44 +0000

    Merged PR 606: fix BatchMatMul translation
    fix BatchMatMul translation

--- src/nnfusion/core/operators/generic_op/generic_op_define/BatchMatMul.cpp
+++ src/nnfusion/core/operators/generic_op/generic_op_define/BatchMatMul.cpp
@@ -62,56 +62,40 @@ REGISTER_OP(BatchMatMul)
         int m0 = input_shape_0[input_shape_0.size() - 2],
             n0 = input_shape_0[input_shape_0.size() - 1];
 
+        int batch = 1;
+        for (int i = 0; i < input_shape_0.size() - 2; ++i)
+            batch *= input_shape_0[i];
+
         auto generic_op = std::dynamic_pointer_cast<nnfusion::op::GenericOp>(gnode->get_op_ptr());
         bool trans_A = generic_op->localOpConfig.getRoot()["adj_x"]["b"];
         bool trans_B = generic_op->localOpConfig.getRoot()["adj_y"]["b"];
-        int k;
-        std::string input0_expr = std::string("args(\"input0\")[");
-        std::string input1_expr = std::string("args(\"input1\")[");
-        if (input_shape_0[0] == 1)
-        {
-            input0_expr += "1";
-        }
-        else
-        {
-            input0_expr += "b";
-        }
-        if (input_shape_1[0] == 1)
-        {
-            input1_expr += "1";
-        }
-        else
-        {
-            input1_expr += "b";
-        }
-        if (!trans_A)
-        {
-            k = n0;
-            input0_expr += ", i, k]";
-        }
-        else
-        {
-            k = m0;
-            input0_expr += ", k, i]";
-        }
 
-        if (!trans_B)
-        {
-            input1_expr += ", k, j]";
-        }
-        else
-        {
-            input1_expr += ", j, k]";
-        }
+        int N = trans_A ? input_shape_0[input_shape_0.size() - 1]
+                        : input_shape_0[input_shape_0.size() - 2];
+        int K = trans_A ? input_shape_0[input_shape_0.size() - 2]
+                        : input_shape_0[input_shape_0.size() - 1];
+        int M = trans_B ? input_shape_1[input_shape_1.size() - 2]
+                        : input_shape_1[input_shape_1.size() - 1];
+
+        std::string input0 = trans_A ? "args(\"input0\")[b, k, n]" : "args(\"input0\")[b, n, k]";
+        std::string input1 = trans_B ? "args(\"input1\")[b, m, k]" : "args(\"input1\")[b, k, m]";
+
+        std::vector<int> shape0 = {batch, N, K};
+        if (trans_A)
+            std::swap(shape0[1], shape0[2]);
+        std::vector<int> shape1 = {batch, K, M};
+        if (trans_B)
+            std::swap(shape1[1], shape1[2]);
+        std::vector<int> outshape = {batch, N, M};
 
         auto expression = op::create_code_from_template(
-            R"( - input("input0", @input_shape_0@); input("input1", @input_shape_1@); k = loop(@k@); output(@output_shape@, lambda b, i, j: tvm.sum(@input0_expr@ * @input1_expr@, axis=k)); )",
-            {{"input_shape_0", vector_to_string(input_shape_0)},
-             {"input_shape_1", vector_to_string(input_shape_1)},
-             {"output_shape", vector_to_string(output_shape)},
-             {"k", k},
-             {"input0_expr", input0_expr},
-             {"input1_expr", input1_expr}});
+            R"( - input("input0", @input_shape_0@); input("input1", @input_shape_1@); k = loop(@k@); output(@output_shape@, lambda b, n, m: tvm.sum(@input0_expr@ * @input1_expr@, axis=k)); )",
+            {{"input_shape_0", vector_to_string(shape0)},
+             {"input_shape_1", vector_to_string(shape1)},
+             {"output_shape", vector_to_string(outshape)},
+             {"k", K},
+             {"input0_expr", input0},
+             {"input1_expr", input1}});
 
         return expression;
     });
