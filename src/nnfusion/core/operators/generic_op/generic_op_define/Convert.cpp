6121c7f059ed1854fb1c13f4caa3ae5802b403b7
Guoshuai Zhao <guzhao@microsoft.com>
2020-05-26 10:08:02 +0000

    Merged PR 611: Fix kernels for HLSL bert inference
    Fix kernels for HLSL bert inference

--- src/nnfusion/core/operators/generic_op/generic_op_define/Convert.cpp
+++ src/nnfusion/core/operators/generic_op/generic_op_define/Convert.cpp
@@ -9,13 +9,21 @@ REGISTER_OP(Convert)
         auto op = static_pointer_cast<nnfusion::op::Convert>(gnode->get_op_ptr());
         NNFUSION_CHECK_NOT_NULLPTR(op) << "Node type is not " << gnode->get_op_ptr()->get_op_type();
 
-        std::string dtype;
+        std::string in_dtype;
         bool ret = element::Type::nnfusion_element_type_to_dtype_string(
-            op->get_convert_element_type(), dtype);
+            gnode->get_input_element_type(0), in_dtype);
+        NNFUSION_CHECK(ret == true) << "cast type is not supported: "
+                                    << gnode->get_input_element_type(0).c_type_string();
+
+        std::string out_dtype;
+        ret = element::Type::nnfusion_element_type_to_dtype_string(op->get_convert_element_type(),
+                                                                   out_dtype);
         NNFUSION_CHECK(ret == true) << "cast type is not supported: "
                                     << op->get_convert_element_type().c_type_string();
 
         return op::create_code_from_template(
-            R"( - input("input0", @input_shape@); output(@input_shape@, topi=topi.cast(args("input0"), dtype="@dtype@")); )",
-            {{"input_shape", vector_to_string(gnode->get_input_shape(0))}, {"dtype", dtype}});
+            R"( - input("input0", @input_shape@, dtype="@in_dtype@"); output(@input_shape@, topi=topi.cast(args("input0"), dtype="@out_dtype@")); )",
+            {{"input_shape", vector_to_string(gnode->get_input_shape(0))},
+             {"in_dtype", in_dtype},
+             {"out_dtype", out_dtype}});
     });
