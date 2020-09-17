8323b6e1285c3e5000ac85442ad2f3527cf03359
Yuqing Xia <yuqxia@microsoft.com>
2020-09-17 06:17:51 +0000

    Merged PR 739: add antares memcpy & pad op inplace annotation
    add antares memcpy & pad op inplace annotation

--- src/nnfusion/core/kernels/cuda_gpu/cuda_emitter.hpp
+++ src/nnfusion/core/kernels/cuda_gpu/cuda_emitter.hpp
@@ -196,18 +196,29 @@ namespace nnfusion
 #endif
                         if (!ir.empty())
                         {
-                            auto info = m_antares_ke_imp->autogen(ir);
-                            antares_code = info.first;
-                            m_is_tuned = info.second;
+                            std::string annotation = nnfusion::op::get_annotation(ir);
+                            // if is_memcpy, no need to request antares server
+                            if (annotation.find("|memcpy|") != string::npos)
+                            {
+                                is_memcpy = true;
+                            }
+                            else
+                            {
+                                auto info = m_antares_ke_imp->autogen(ir);
+                                antares_code = info.first;
+                                m_is_tuned = info.second;
+                            }
                         }
                     }
                 }
 
+                bool is_eliminative() override;
                 LanguageUnit_p emit_function_body() override;
                 LanguageUnit_p emit_dependency() override;
                 void set_launch_config() override {}
                 AntaresKEImp::Pointer m_antares_ke_imp;
                 std::string antares_code;
+                bool is_memcpy;
             };
 
         } // namespace cuda
