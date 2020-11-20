78523b0e8908c64b7ccba1ce084d6a937dcd7fc1
Ze Gao <v-zegao@microsoft.com>
2020-11-20 10:36:02 +0800

    enable cmake to auto find superscaler lib

--- thirdparty/superscaler/superscaler.cmake
+++ thirdparty/superscaler/superscaler.cmake
@@ -1,15 +1,33 @@
-#platform-aware setting
-if(CMAKE_SYSTEM_NAME MATCHES "Linux")
-	set(SUPERSCALER_NAME "libsuperscaler.${CMAKE_SYSTEM_NAME}.${CMAKE_SYSTEM_PROCESSOR}.${TARGET_GPU_PLATFORM}.so")
-	set(SUPERSCALER_PLATFORM_NAME "libtfadaptor.so")
-else()
-	message(FATAL "Not Supported Yet")
-endif()
-
 execute_process(
-	COMMAND ${CMAKE_COMMAND} -E create_symlink ${SUPERSCALER_NAME} ${SUPERSCALER_PLATFORM_NAME} WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-)
+  COMMAND
+    python -c
+    "from distutils.sysconfig import get_python_lib; print(get_python_lib())"
+  OUTPUT_VARIABLE PYTHON_SITE_PACKAGES
+  OUTPUT_STRIP_TRAILING_WHITESPACE)
+message(STATUS ${PYTHON_SITE_PACKAGES})
+
+find_path(
+  SUPERSCALER_INCLUDE_DIR
+  NAMES superscaler.h
+  HINTS ${CMAKE_CURRENT_SOURCE_DIR}/superscaler)
+
+find_library(
+  SUPERSCALER_LIBRARY
+  NAMES superscaler_pywrap
+  HINTS ${PYTHON_SITE_PACKAGES}
+  PATH_SUFFIXES superscaler/lib)
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set VILLASNODE_FOUND to TRUE if
+# all listed variables are TRUE
+find_package_handle_standard_args(superscaler DEFAULT_MSG SUPERSCALER_LIBRARY)
+
+mark_as_advanced(SUPERSCALER_INCLUDE_DIR SUPERSCALER_LIBRARY)
+
+set(SUPERSCALER_LIBRARIES ${SUPERSCALER_LIBRARY})
+set(SUPERSCALER_INCLUDE_DIRS ${SUPERSCALER_INCLUDE_DIR})
 
 add_library(superscaler INTERFACE)
-target_include_directories(superscaler SYSTEM INTERFACE "${CMAKE_CURRENT_LIST_DIR}")
-target_link_libraries(superscaler INTERFACE "${CMAKE_CURRENT_LIST_DIR}/${SUPERSCALER_PLATFORM_NAME}")
+target_include_directories(superscaler SYSTEM
+                           INTERFACE ${SUPERSCALER_INCLUDE_DIRS})
+target_link_libraries(superscaler INTERFACE ${SUPERSCALER_LIBRARIES})
